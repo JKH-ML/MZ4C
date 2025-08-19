b3ddeb36399372d2506bc9b057ee27aa3d733bb9
JKH-ML <banghak2da@gmail.com>
2025-08-19 22:46:36 +0900

    기본 구조

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   images: {
     domains: ['open.api.nexon.com'],
   },
+  // React의 strict mode 비활성화 (hydration 오류 완화)
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
