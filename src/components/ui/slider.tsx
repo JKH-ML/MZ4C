115a8f056dce6fa469bd63056ac5d1b997a67fca
JKH-ML <banghak2da@gmail.com>
2025-08-22 14:23:12 +0900

    fix: Slider 컴포넌트 TypeScript 타입 오류 수정

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface SliderProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface SliderProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
   min?: number
   max?: number
   step?: number
